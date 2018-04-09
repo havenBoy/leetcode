98232b8ca2bc5c7c32f01d369c7100d4d1cf0f08
doco <xhao.wang@foxmail.com>
2018-04-09 17:17:55 +0800

    twoSum

--- LeetCode/src/com/xiong/test/TwoSum.java
+++ LeetCode/src/com/xiong/test/TwoSum.java
@@ -1,10 +1,16 @@
 package com.xiong.test;
 
+import java.util.HashMap;
+
 public class TwoSum {
 	
+	/*
+	 * 有且只有一个，只要找出立马跳出循环
+	 * */
+	//时间复杂度为n2
 	public static int[] twoSum(int[] nums, int target) {
-		 int[] array = new int[2];
-	     for (int i = 0; i < nums.length-1; i++) {
+		int[] array = new int[2];
+		for (int i = 0; i < nums.length-1; i++) {
 			for (int j = i+1; j < nums.length; j++) {
 				if (nums[i] + nums[j] == target) {
 					array[0] = i;
@@ -13,14 +19,31 @@ public class TwoSum {
 				}
 			}
 		}
-	    return array;
+		return array;
+	}
+	
+	//另外一种是以一个数字为准，找到数组中另外的一个数字，那一招一
+	public static int[] twoSum1(int[] nums,int target) {
+		int[] array = new int[2];
+		HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			map.put(nums[i], i+1);
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(target-nums[i])) {
+				array[0] = i;
+				array[1] = map.get(target-nums[i]);
+				break;
+			}
+		}
+		return array;
 	}
 	
 	public static void main(String[] args) {
-		
 		int[] nums = {2, 7, 11, 15};
 		int target = 18;
 		int[] array = twoSum(nums, target);
+		//int[] array = twoSum1(nums, target);
 		System.out.println(array[0] + ";" + array[1]);
 	}
 
