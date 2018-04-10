f6db53c6c035820d44c6baff551f2db54ff24bdc
doco <xhao.wang@foxmail.com>
2018-04-10 11:38:56 +0800

    二叉树的定义

---
+++ LeetCode/src/com/xiong/test/TreeNode.java
@@ -0,0 +1,14 @@
+package com.xiong.test;
+
+/**
+ * 二叉树的定义
+ * @author xiaoxiong
+ *
+ */
+public class TreeNode {
+	
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
