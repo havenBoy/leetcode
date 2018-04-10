9d3d9a052f2536e7d8471a09632de154711ee839
doco <xhao.wang@foxmail.com>
2018-04-10 18:14:41 +0800

    计算一个数字的平方根

---
+++ LeetCode/src/com/xiong/test/SqurtNum.java
@@ -0,0 +1,28 @@
+package com.xiong.test;
+
+public class SqurtNum {
+	/**
+	 * 算出一个数字的平方根
+	 */
+/*	public static int getSqrt(int num) {
+		int low=1,high=num;
+		while(low <= num) {
+			int x = (low + high)/2;
+			if (x*x == num) {
+				return x;
+			} else if (x*x < num){
+				high = x-1;
+			} else {
+				low = x+1;
+			}
+		}
+		return high;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(getSqrt(10));
+		
+	}*/
+
+}
