ab376757ade5756f9d197cb6bd2a264d3d7ebc18
havenBoy <18666932076@163.com>
2018-04-11 17:37:39 +0800

    增加二叉树的非递归遍历

--- LeetCode/src/com/xiong/test/TreeNode.java
+++ LeetCode/src/com/xiong/test/TreeNode.java
@@ -1,5 +1,7 @@
 package com.xiong.test;
 
+import java.util.Stack;
+
 /**
  * 二叉树的定义
  * @author xiaoxiong
@@ -11,6 +13,27 @@ public class TreeNode {
     TreeNode left;
     TreeNode right;
     TreeNode(int x) { val = x; }
+    
+    /***
+     * 非递归实现
+     * @param node
+     */
+    public static void preNode1(TreeNode node) {
+		Stack<TreeNode> stack = new Stack<>();
+    	if (node != null) {
+			stack.push(node);
+		}
+    	while(stack.size() != 0) {
+            TreeNode p = stack.pop();
+            System.out.println(p.val);
+            if (node.left != null) stack.push(p.left);
+            if (node.right != null) stack.push(p.right);
+    	}
+	}
+    
+    
+    
+    
     //前序遍历
     public static void preNode(TreeNode node) {
 		if (node != null) {
