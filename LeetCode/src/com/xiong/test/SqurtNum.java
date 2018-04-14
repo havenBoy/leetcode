7a4d3de73e83182ad41dc92ff9d032233071a033
havenBoy <18666932076@163.com>
2018-04-14 23:00:28 +0800

    计算一个数字的平方根以及判断一个数字是否是2的幂次方

--- LeetCode/src/com/xiong/test/SqurtNum.java
+++ LeetCode/src/com/xiong/test/SqurtNum.java
@@ -1,16 +1,45 @@
 package com.xiong.test;
 
 public class SqurtNum {
+	
+	/***
+	 * 判断一个数字是否是2的幂次方
+	 * 最原始的办法，但是时间复杂度太高
+	 * @param n
+	 * @return
+	 */
+	
+	public static boolean isPowerOfTwo(int n) {
+		int num = 1;
+		if (n == 1) return true;
+		while (num < n && n %2 == 0) {
+			num = num << 1;
+			if (num == n) return true;
+		}
+		return false;
+	}
+	
+	/***
+	 * 最简单的办法
+	 * @param n
+	 * @return 
+	 */
+	public static boolean isPowerOfTwo2(int n) {
+		
+		return n > 0 ? (n & (n-1)) == 0 : false;
+	}
+	
 	/**
+	 * @param num
 	 * 算出一个数字的平方根
 	 */
-/*	public static int getSqrt(int num) {
+	public static int getSqrt(int num) {
 		int low=1,high=num;
-		while(low <= num) {
+		while(low <= high) {
 			int x = (low + high)/2;
 			if (x*x == num) {
 				return x;
-			} else if (x*x < num){
+			} else if (x*x > num){ 
 				high = x-1;
 			} else {
 				low = x+1;
@@ -20,9 +49,8 @@ public class SqurtNum {
 	}
 	
 	public static void main(String[] args) {
-		
-		System.out.println(getSqrt(10));
-		
-	}*/
+		System.out.println(getSqrt(99));
+		System.out.println(isPowerOfTwo(128));
+	}
 
 }
