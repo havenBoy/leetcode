31b5916c812ef1e05d262fda724b206b5ed42364
havenBoy <18666932076@163.com>
2018-04-20 17:43:02 +0800

    数组向右移动K后的数组

---
+++ LeetCode/src/com/xiong/test/MoveK.java
@@ -0,0 +1,30 @@
+package com.xiong.test;
+/***
+ * @author Xiong
+ * 数组向右移动K位后的数组  1,2,3,4  2位    3,4,1,2
+ */
+public class MoveK {
+	
+	public static int[] reverse(int[] nums, int i, int j) {
+		while(i < j) {
+			int temp = nums[i];
+			nums[i] = nums[j];
+			nums[j] = temp;
+			i++;
+			j--;
+		}
+		return nums;
+	}
+	
+	public static int[] move(int[] nums, int k) {
+		nums = reverse(nums, 0, nums.length-1-k);
+		nums = reverse(nums, nums.length-k, nums.length-1);
+		return reverse(nums, 0, nums.length-1);
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,4,5};
+		Utils.show(move(nums, 3));
+	}
+
+}
