e7238c5645111955ece8f6884d2b6a866d6c7027
havenBoy <18666932076@163.com>
2018-04-20 17:25:43 +0800

    something to do

--- LeetCode/src/com/xiong/test/RemoveElement.java
+++ LeetCode/src/com/xiong/test/RemoveElement.java
@@ -8,7 +8,8 @@ public class RemoveElement {
 	
 	/**
 	 * Given nums = [1,1,1,2,2],
-	   Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+	   Your function should return length = 2, 
+	   with the first two elements of nums being 1 and 2 respectively.
 	   It doesn't matter what you leave beyond the new length.
 	 * @param nums
 	 * @return
@@ -22,10 +23,32 @@ public class RemoveElement {
 		return set.size();
 	}
 	
+	/***
+	 * Given nums = [3,2,2,3], val = 3,
+     * Your function should return length = 2, with the first two elements of nums being 2.
+     * It doesn't matter what you leave beyond the returned length.
+	 * @param nums
+	 * @param val
+	 * @return
+	 */
+	public static int remove(int[] nums, int val) {
+		int count=0 ,index = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == val) {
+				int temp = nums[i];
+				nums[i] = nums[index];
+				nums[index] = temp;
+				count++;
+			}
+		}
+		return nums.length-count;
+	}
+	
 	
 	
 	public static void main(String[] args) {
-		
+		int[] num = {3,2,2,3};
+		System.out.println(remove(num, 3));
 		int[] nums = {1,1,1,2,2};
 		System.out.println(removeElement(nums));
 	}
