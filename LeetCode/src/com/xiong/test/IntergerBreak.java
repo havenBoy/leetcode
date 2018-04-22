7558b56adf2c59b08463ae5ce38842a6e0e1377c
havenBoy <18666932076@163.com>
2018-04-22 23:40:48 +0800

    add solution todo

---
+++ LeetCode/src/com/xiong/test/IntergerBreak.java
@@ -0,0 +1,13 @@
+package com.xiong.test;
+/**
+ * @author 作者:XiaoXiong
+ * @version 创建时间：2018年4月22日 下午10:45:49
+ * 类说明
+ */
+public class IntergerBreak {
+	
+	public static int intergerBreak(int n) {
+		return 0;
+	}
+
+}
