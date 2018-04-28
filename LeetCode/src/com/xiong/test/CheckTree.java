b0f173c97e010d928cfa62fc01006f31a119d0ff
havenBoy <18666932076@163.com>
2018-04-28 09:46:26 +0800

    add treeNode problem solution

--- LeetCode/src/com/xiong/test/CheckTree.java
+++ LeetCode/src/com/xiong/test/CheckTree.java
@@ -13,5 +13,29 @@ public class CheckTree {
 		}
 		return false;
 	}
+	/***
+	 * 二叉树的最大深度
+	 * @param node
+	 * @return
+	 */
+	public static int maxDepth(TreeNode node) {
+		if (node == null) return 0;
+		int left = maxDepth(node.left) + 1;
+		int right = maxDepth(node.right) + 1;
+		return left > right ? left : right ;
+	}
+	/***
+	 * 返回二叉树的最小深度
+	 * @param node
+	 * @return
+	 */
+	public static int minDepth(TreeNode node) {
+		if (node == null) return 0;
+		if (node.left == null)  return minDepth(node.right) + 1;
+		if (node.right == null) return minDepth(node.left) + 1;
+		int left = minDepth(node.left) + 1;
+		int right = minDepth(node.right) + 1;
+		return left > right ? right : left ;
+	}
 
 }
