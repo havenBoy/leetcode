aac43cb60e37871baa99455fff4ea92c10639881
havenBoy <18666932076@163.com>
2018-05-03 23:23:15 +0800

    add remove kth listnode todo

--- LeetCode/src/com/xiong/test/ListProblems.java
+++ LeetCode/src/com/xiong/test/ListProblems.java
@@ -54,24 +54,40 @@ public class ListProblems {
 	
 	/***
 	 * 遍历一遍得到链表的倒数第K个节点
+	 * 删除列表的倒数第K个节点
 	 * @param node
 	 * @return
 	 */
 	
-	public static ListNode getK(ListNode node) {
-		return null;
+	public static ListNode getK(ListNode head, int k) {
+		if(head == null || k <= 0)return head; 
+		ListNode headNode = new ListNode(0);
+		headNode.next = head;
+		ListNode first = head;
+		ListNode last = head;
+		while(k > 0){
+			first = first.next;
+			k--;
+		}
+		while(first.next != null){
+			last = last.next;
+			first = first.next;
+		}
+		if (last.next != null)  last.next = last.next.next;
+		return headNode.next;
 	}
 	
 	public static void main(String[] args) {
 		ListNode node1 = new ListNode(1);
-		ListNode node2 = new ListNode(1);
+		ListNode node2 = new ListNode(5);
 		ListNode node3 = new ListNode(3);
 		ListNode node4 = new ListNode(4);
 		node1.next = node2;
 		node2.next = node3;
 		node3.next = node4;
 		node4.next = null;
-		ListNode.show(delete1(node1, 1));
+		//ListNode.show(delete1(node1, 1));
+		ListNode.show(getK(node1, 4));
 		//ListNode.show(deleteDuplicates(node1));
 	}
 
