7aac0c9f42bb85bf8e0beea03b523bfa39aba392
havenBoy <18666932076@163.com>
2018-05-23 15:44:52 +0800

    add string parse num solution

--- LeetCode/src/com/xiong/test/PowFun.java
+++ LeetCode/src/com/xiong/test/PowFun.java
@@ -23,8 +23,18 @@ public class PowFun {
 		return (n%2 == 0) ? powfun(num*num, n/2) : num*powfun(num*num, n/2);
 	}
 	
+	/**
+	 * 判断一个字符串可以转化为数字
+	 * @param s
+	 * @return
+	 */
+	public static boolean isNumber(String s) {
+		return s.matches("^\\s*[-+]?((\\d+(\\.\\d*)?)|(\\.\\d+))(e[-+]?\\d+)?\\s*$");
+	}
+
+	
 	public static void main(String[] args) {
-		
+		System.out.println(isNumber("2e10"));
 		System.out.println(powfun(3.00, 5));
 		System.out.println(powfun(3.00, 0));
 		System.out.println(powfun(3.00, -5));
