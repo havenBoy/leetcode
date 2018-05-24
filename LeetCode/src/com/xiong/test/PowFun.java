83024be49bafae7973cd393d064bce71135f1410
havenBoy <18666932076@163.com>
2018-05-24 11:11:42 +0800

    add reverse intenger

--- LeetCode/src/com/xiong/test/PowFun.java
+++ LeetCode/src/com/xiong/test/PowFun.java
@@ -3,6 +3,25 @@ package com.xiong.test;
 public class PowFun {
 	
 	/**
+	 * 整数的反转
+	 * @param x
+	 * @return
+	 */
+	public static int reverse(int x) {
+		long result = 0;
+		long num = x;
+		if(num < 0) num = -num;
+		if(num > Integer.MAX_VALUE) return 0;
+		while(num >= 1) {
+			result = num % 10 + result * 10;
+			num /= 10;
+		}
+		result = result > Integer.MAX_VALUE ? 0 : result;
+		result = x < 0 ? -result : result;
+		return (int)result;
+	}
+	
+	/**
 	 * Implement pow(x, n).
 	 *	Example 1:
 	 * 	Input: 2.00000, 10
@@ -10,7 +29,6 @@ public class PowFun {
 	 *	Example 2:
 	 *	Input: 2.10000, 3
 	 *	Output: 9.26100
-	 * 
 	 * */
 	
 	public static double powfun(double num,int n) {
@@ -34,6 +52,7 @@ public class PowFun {
 
 	
 	public static void main(String[] args) {
+		System.out.println(reverse(-1563847412));
 		System.out.println(isNumber("2e10"));
 		System.out.println(powfun(3.00, 5));
 		System.out.println(powfun(3.00, 0));
