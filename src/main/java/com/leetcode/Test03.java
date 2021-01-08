e361f7b0e71dfa85aef73821f4151ec641a58072
havenBoy <18666932076@163.com>
2021-01-08 21:49:53 +0800

    [FIX]最大面积代码优化

--- src/main/java/com/leetcode/Test03.java
+++ src/main/java/com/leetcode/Test03.java
@@ -84,13 +84,17 @@ public class Test03 {
         return true;
     }
 
+    /**
+     * 最大区域面积  二分策略调整
+     * 思路：首先尝试获取到最大的长，可以获取到当前长度下的面积
+     * 要获取到更大的面积，需要试着调整长，调整长的策略：
+     * 保留当前最大的高，移动较小值的下标重新计算面积大小。
+     * @param height  输入的数组
+     * @return 返回最大面积数字
+     */
     public static int maxArea(int[] height) {
         int length = height.length;
-        if (length <= 1) {
-            return 0;
-        }
-        int start = 0;
-        int end = length - 1;
+        int start = 0, end = length - 1;
         int result = 0;
         while (start < end) {
             int startNum = height[start];
@@ -234,7 +238,7 @@ public class Test03 {
 
         //int[] nums = {1,8,6,2,5,4,8,3,7};
 
-        int[] nums = {1,1};
+        int[] nums = {1};
         System.out.println(maxArea(nums));
     }
 }
