c3036931613f848590d43896576b8cb734117d62
zhaoxiaoxiong <18666932076@163.com>
2024-05-20 11:27:02 +0800

    [ADD]贪心算法思想添加

--- src/main/java/com/leetcode/leet_problems/DP/Test322_coinChange.java
+++ src/main/java/com/leetcode/leet_problems/DP/Test322_coinChange.java
@@ -1,5 +1,7 @@
 package com.leetcode.leet_problems.DP;
 
+import java.util.Arrays;
+
 public class Test322_coinChange {
 
     /*
@@ -32,14 +34,62 @@ public class Test322_coinChange {
         return res == Integer.MAX_VALUE ? -1 : res;
     }
 
-    public static int coin(int[] arr, int target) {
-        return coinChange(arr, target);
+    /**
+     * 贪心算法思想
+     * @param target 目标值
+     * @param arr 给定数组
+     * @return 返回最少需要的硬币数量
+     */
+    public static int coinChange2(int[] arr, int target) {
+        if (target == 0 || arr.length == 0) {
+            return 0;
+        }
+        if (target < 0) {
+            return -1;
+        }
+        Arrays.sort(arr);
+        int res = target, index = 0;
+        while (index < arr.length) {
+            int count = 0;
+            for (int i = arr.length - ++index; i >= 0; i--) {
+                if (target >= arr[i]) {
+                    int cc = target / arr[i];
+                    target %= arr[i];
+                    count += cc;
+                }
+                if (target == 0) {
+                    return count;
+                }
+            }
+            target = res;
+        }
+        return -1;
     }
 
+    public static int coinChange11(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        for (int i = 0; i <= target; i++) {
+            dp[i] = target + 1;
+        }
+        dp[0] = 0;
+        for (int i = 1; i <= target; i++) {
+            for (int j : nums) {
+                if (i - j < 0) {
+                    continue;
+                }
+                dp[i] = Math.min(dp[i], 1 + dp[i - j]);
+            }
+        }
+        return (dp[target] == target + 1) ? -1 : dp[target];
+    }
+
+
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 5};
-        int target = 16;
-        System.out.println(coin(arr, target));
+        int[] arr = new int[]{186,419,83,408};
+        int target = 6249;
+//        System.out.println(coinChange(arr, target));
+//        System.out.println(coinChange2(arr, target));
+        System.out.println(coinChange11(arr, target));
     }
 
 }
